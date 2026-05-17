0e346fcfc7585cf8ffb0623ef6fcd54d7e8f6cc1
kaoumar689-droid <kaoumar689@gmail.com>
2026-05-17 11:15:22 +0100

    fix: hardcode supabase credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+    'https://cdihkzydctnnegkfpooo.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkaWhrenlkY3RubmVna2Zwb29vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Nzc4MDYsImV4cCI6MjA5NDQ1MzgwNn0.yR8_pzfQSQao2wkE4jtCWj3JzmfPa8_fMAP8z7GHXr8'
+)
