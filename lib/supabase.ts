1a0a97ce47b263618d7922a01b47d3c079664626
kaoumar689-droid <kaoumar689@gmail.com>
2026-06-07 22:55:03 +0100

    fix: supabase utilise les variables env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    'https://cdihkzydctnnegkfpooo.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkaWhrenlkY3RubmVna2Zwb29vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Nzc4MDYsImV4cCI6MjA5NDQ1MzgwNn0.yR8_pzfQSQao2wkE4jtCWj3JzmfPa8_fMAP8z7GHXr8'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
